e67f0820209e0d486f9253c988d8561479bd5bce
mdos-san <mdossan@protonmail.com>
2023-03-25 14:05:44 +0100

    feat: Add population module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod agent;
+pub mod population;
 pub mod dna;
 pub mod genetic_algorithm;
---
+++ src/population/mod.rs
@@ -0,0 +1,34 @@
+use crate::agent::Agent;
+
+pub struct Population<T> {
+    agents: Vec<Agent<T>>,
+}
+
+impl<T> Population<T> {
+    pub fn new(size: usize, generate_dna: fn() -> Vec<T>) -> Population<T> {
+        let mut new_population = Population { agents: vec![] };
+
+        for _ in 0..size {
+            new_population.agents.push(Agent::new(generate_dna(), 0.0));
+        }
+
+        return new_population;
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use crate::population::Population;
+
+    fn generate_dna() -> Vec<i32> {
+        return vec![1, 2, 3];
+    }
+
+    #[test]
+    fn can_create_a_population() {
+        let population = Population::new(1024, generate_dna);
+        assert_eq!(population.agents.len(), 1024);
+        assert_eq!(population.agents.get(0).unwrap().dna.len(), 3);
+        assert_eq!(population.agents.get(1023).unwrap().dna.len(), 3);
+    }
+}
